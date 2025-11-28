4a83246db19a99fab8c1cc1325cfaefa8ff98842
vickbk <vickbake8@gmail.com>
2025-11-28 20:23:30 +0200

    Adding sass support and initing color and fonts

--- src/components/cart/EmptyCart.tsx
+++ src/components/cart/EmptyCart.tsx
@@ -3,12 +3,13 @@ import { Heading } from "../shared/Heading";
 
 export const EmptyCart = () => {
   return (
-    <Article>
-      <img
-        src="/assets/images/illustration-empty-cart.svg"
-        alt="Empty cart illustration"
-      />
+    <Article className="grid justify-items-center">
       <Heading>Your added items will appear here.</Heading>
+      <img
+        className="row-start-1"
+        src="/assets/images/illustration-empty-cart.svg"
+        alt="Your cart is empty for now."
+      />
     </Article>
   );
 };
