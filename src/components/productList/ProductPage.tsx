667e64eddeb78ab359856c19812a0ee84d98dd52
vickbk <vickbake8@gmail.com>
2025-11-29 11:03:34 +0200

    initial styling of the product page

--- src/components/productList/ProductPage.tsx
+++ src/components/productList/ProductPage.tsx
@@ -2,7 +2,7 @@ import { ProductElement } from "./productElement/ProductElement";
 
 export const ProductPage = () => {
   return (
-    <div>
+    <div className="grid gap-4">
       <ProductElement />
     </div>
   );
